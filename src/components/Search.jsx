01d49f35ab58bec54225e01894d0abb12b5fd1ed
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-01 15:29:30 -0400

    Requisito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  state = {
+    artist: '',
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { artist } = this.state;
+    const isDisable = artist.length < 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <div>
+          <input
+            data-testid="search-artist-input"
+            value={ artist }
+            type="text"
+            name="artist"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ isDisable }
+          >
+            Procurar
+          </button>
+        </div>
       </div>
     );
   }
