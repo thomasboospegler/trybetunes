a460bb6d9ed743b210c9974fd031b8f146968655
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-01 14:33:59 -0400

    Requisito 2

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
+import Loading from './components/Loading';
+import { createUser } from './services/userAPI';
+
+class App extends React.Component {
+  state = {
+    userName: '',
+    loading: false,
+    loggedIn: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      console.log('aaa');
+      const { userName } = this.state;
+      await createUser({ name: userName });
+      this.setState({
+        loading: false,
+        loggedIn: true,
+      });
+    });
+  }
+
+  render() {
+    const { userName, loading, loggedIn } = this.state;
+    return (
+      <div>
+        <h1>TrybeTunes</h1>
+        {loggedIn && <Redirect to="/search" />}
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ (propsRouter) => (
+              !loading ? <Login
+                { ...propsRouter }
+                userName={ userName }
+                name="userName"
+                onChange={ this.handleChange }
+                onSubmit={ this.handleSubmit }
+              /> : <Loading />
+            ) }
+          />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Login extends Component {
   render() {
+    const { userName, onChange, name, onSubmit } = this.props;
+    const userMinLength = 3;
+    const isDisable = userName.length < userMinLength;
     return (
       <div data-testid="page-login">
-        Login
+        <input
+          type="text"
+          data-testid="login-name-input"
+          name={ name }
+          value={ userName }
+          onChange={ onChange }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isDisable }
+          onClick={ onSubmit }
+        >
+          Entrar
+
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  userName: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
